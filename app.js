109eeb1b3fc9614fb4cd53e599a4d80a1489c7f1
bannon <marijunjnja@gmail.com>
2019-08-29 21:08:25 -0500

    console tracker up

--- app.js
+++ app.js
@@ -1,41 +1,27 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const got = require('got')
+const geolib = require('geolib')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const delay = 10
+const url = 'https://api.open-notify.org/iss-now.json'
+const myPosition = {
+  latitude: 30.2077,
+  longitude: -92.0656
+}
 
-var app = express();
+function loop() {
+  got(url, { json: true })
+    .then(iss => {
+      const position = iss.body.iss_position
+      const distanceFromIss = geolib.getDistance(myPosition, position)
+      const distanceFromIssMiles = geolib.convertDistance(distanceFromIss, 'mi')
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+      console.log(`${distanceFromIssMiles} miles`)
+    })
+    .catch((err) => {
+      console.log(err.response.body)
+    }
+  )
+  setTimeout(loop, delay * 1000)
+}
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+loop()
