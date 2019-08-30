1d02d1b0b943bd4d22844e9aa7d760bb5aedf0ef
bannon <marijunjnja@gmail.com>
2019-08-29 21:09:53 -0500

    changed from https to http

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const got = require('got')
 const geolib = require('geolib')
 
 const delay = 10
-const url = 'https://api.open-notify.org/iss-now.json'
+const url = 'http://api.open-notify.org/iss-now.json'
 const myPosition = {
   latitude: 30.2077,
   longitude: -92.0656
