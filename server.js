8d245dc07412fb44fa4f1dddd563e94cacb6672e
bannon <marijunjnja@gmail.com>
2019-09-08 11:55:10 -0500

    cleaned up comments

--- server.js
+++ server.js
@@ -17,72 +17,7 @@ const distance = async () => {
   return await issTracker.getDistance.then((res) => {
     console.log(`res2: ${res}`)
     makeServer(res)
-    // return res
   })
 }
 
 distance()
-
-
-
-
-
-
-
-
-
-// let realDistance = async () => {
-//   return await distance().then((res) => { 
-//     console.log(`res1: ${res}`)
-//     return res 
-//   })
-// }
-// realDistance()
-
-
-
-// const issTracker = require('./issTrack')
-
-// console.log(issTracker())
-
-// const getIss = async () => {
-//   return await issTracker().getDistance
-// }
-
-// let distance = await getIss()
-// console.log(distance)
-
-// console.log(getIss())
-
-// getIss()
-// .then((res) => {
-//   console.log(res)
-// })
-
-
-    // .then((res) => {
-    //   console.log(res)
-    // })
-    // .catch((err) => {
-    //   console.log(err)
-    // })
-
-// console.log(distance())
-
-
-
-
-// const realDistance = await distance()
-
-// let realDistance = async () => {
-//   return await distance()
-//   .then((res) => { 
-//     console.log(`res2: ${res}`)
-//   })
-// }
-
-// console.log(`realDistance: ${realDistance()}`)
-
-// .then((res) => {
-//   console.log(res)
-// })
